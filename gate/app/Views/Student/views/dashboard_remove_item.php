b6920328e9fd3b211c36dca3a2a33849fa0a5718
Lighborn-Gusion <lightborn.granger12@gmail.com>
2026-07-06 22:48:49 +0800

    icon update

--- gate/app/Views/Student/views/dashboard_remove_item.php
+++ gate/app/Views/Student/views/dashboard_remove_item.php
@@ -35,7 +35,7 @@ $layout = service('request')->hasHeader('HX-Request') ? 'Student/layout/htmx' : 
 
                     <?php if (empty($approvedItems)): ?>
                         <div class="text-center text-muted py-4">
-                            <i class="ti ti-device-laptop fs-1 d-block mb-2"></i>
+                            <i class="ti ti-device-laptop d-block mb-3 text-muted" style="font-size: 2rem; opacity: 0.5;"></i>
                             No active items available to unregister.
                         </div>
                     <?php else: ?>
