c7a04c89fe21c484cae568bb06c84e3689d20731
Lighborn-Gusion <lightborn.granger12@gmail.com>
2026-07-20 02:16:45 +0800

    fixed student reason for unregistration

--- gate/app/Models/StudentItemModel.php
+++ gate/app/Models/StudentItemModel.php
@@ -20,9 +20,11 @@ class StudentItemModel extends Model
         'photo',
         'status',
         'notes',
+        'reason',
         'created_at',
         'updated_at',
         'rfid',
-        'in_campus'
+        'in_campus',
+        'resolved_at'
     ];
 }
