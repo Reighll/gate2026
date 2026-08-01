9438d5172992d7d51a8ac33a196502325cc8c824
Lighborn-Gusion <lightborn.granger12@gmail.com>
2026-08-01 18:49:30 +0800

    add report resolved item logs

--- gate/app/Views/Admin/views/item_reports.php
+++ gate/app/Views/Admin/views/item_reports.php
@@ -126,7 +126,7 @@
                                         <?php if ($isResolved): ?>
                                             <span class="badge bg-success rounded-pill">RESOLVED</span>
                                         <?php else: ?>
-                                            <span class="badge bg-danger rounded-pill">RESOLVED</span>
+                                            <span class="badge bg-danger rounded-pill">ONGOING</span>
                                         <?php endif; ?>
                                     </td>
                                 </tr>
